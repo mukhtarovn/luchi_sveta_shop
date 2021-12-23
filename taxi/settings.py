5dbed07d8ff500ba85a5f7fb294845a29f69a379
nariman <mukhtarov.n@gmail.com>
2021-12-24 00:37:24 +0300

    change databases

--- taxi/settings.py
+++ taxi/settings.py
@@ -78,17 +78,9 @@ AUTH_USER_MODEL = 'authapp.ShopUser'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-     #   'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
-        
-    #}
     'default': {
-        'NAME': 'nariman',
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'nariman',
-        'PASSWORD': 'sarvan030511',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 
 }
