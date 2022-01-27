8f1034ab280050d2fb2e36f14be22e1271cd31a3
nariman <mukhtarov.n@gmail.com>
2022-01-27 21:25:29 +0300

    change db

--- taxi/settings.py
+++ taxi/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     #}
     
     'default': {
-        'NAME': 'luchi_saveta',
+        'NAME': 'luchi_sveta',
         'ENGINE': 'django.db.backends.postgresql',
         'USER': 'nariman',
         'PASSWORD': 'sarvan030511'
