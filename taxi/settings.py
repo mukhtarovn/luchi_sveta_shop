a2f9378609c19772eab6d6b2470a5b158b481872
nariman <mukhtarov.n@gmail.com>
2022-02-21 00:09:58 +0300

    change contacts, add delivery info

--- taxi/settings.py
+++ taxi/settings.py
@@ -79,8 +79,8 @@ AUTH_USER_MODEL = 'authapp.ShopUser'
 
 DATABASES = {
     #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
+     #   'ENGINE': 'django.db.backends.sqlite3',
+     #   'NAME': BASE_DIR / 'db.sqlite3',
     #}
     
     'default': {
