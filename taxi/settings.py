ba30ef48c71ab921097b94c15984768b64a4614b
nariman <mukhtarov.n@gmail.com>
2022-04-28 19:57:14 +0300

    add toolbar

--- taxi/settings.py
+++ taxi/settings.py
@@ -155,14 +155,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
-#STATICFILES_DIRS = (
-   # os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
